6614e39e90315a165edc48391ff8d39274e7ceda
yaroslav gubich <yar.mansens@gmail.com>
2024-12-04 13:32:54 +0100

    created simple product page

---
+++ pages/product/[slug].js
@@ -0,0 +1,9 @@
+import React from "react";
+// not working, can't find this module
+import { link, client } from "../../app/lib/client";
+
+const ProductDetails = () => {
+  return <div>Product</div>;
+};
+
+export default ProductDetails;
