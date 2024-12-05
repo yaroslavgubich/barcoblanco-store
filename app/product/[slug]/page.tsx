02937d6758791fd41cbecf16b805570d286134bb
yaroslav gubich <yar.mansens@gmail.com>
2024-12-05 14:28:42 +0100

    add MUI product details card

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,9 +1,39 @@
-import React from "react";
-// not working, can't find this module
-// import { link, client } from "../../app/lib/client";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardActions from "@mui/material/CardActions";
 
 const ProductDetails = () => {
-  return <div>TEST SLUG</div>;
+  return (
+    <Card sx={{ maxWidth: 345, outline: `2px solid red` }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default ProductDetails;
