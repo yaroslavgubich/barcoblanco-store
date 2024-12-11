e4d0e7e6589b5b531b0437742a7c1a03e8879edd
yaroslav gubich <yar.mansens@gmail.com>
2024-12-11 16:35:00 +0100

    add navbar with drawer menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
           <Navbar />
         </header>
         <main className="main-container">{children}</main>
-        <footer>
+        <footer id="footer">
           <Footer />
         </footer>
       </body>
--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,4 +1,9 @@
-import * as React from "react";
+//import sanity and product 
+"use client";
+import React from "react";
+import { client } from "../../lib/client";
+import Product from "../../components/Product";
+//import MUI
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -7,9 +12,9 @@ import Button from "@mui/material/Button";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardActions from "@mui/material/CardActions";
 
-const ProductDetails = () => {
+export default function MultiActionAreaCard() {
   return (
-    <Card sx={{ maxWidth: 345, outline: `2px solid red` }}>
+    <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
@@ -28,12 +33,8 @@ const ProductDetails = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
+        <Button variant="contained">Order</Button>
       </CardActions>
     </Card>
   );
-};
-
-export default ProductDetails;
+}
