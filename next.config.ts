a49887884ef58e338ecd61d508317997f08f7b3b
yaroslav gubich <yar.mansens@gmail.com>
2025-01-19 08:44:10 +0100

    add next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
