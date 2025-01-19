187f0d3903443858c68f87201341270be6d66b28
yaroslav gubich <yar.mansens@gmail.com>
2025-01-19 05:19:26 +0100

    add api and page.tsx

---
+++ app/api/categories/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import { client } from "../../client";
+
+export async function GET() {
+  const categories = await client.fetch(
+    `*[_type == "category"]{_id, title, slug}`
+  );
+  return NextResponse.json(categories);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,36 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from "react";
+import {
+  HeroBanner,
+  Product,
+  AboutSection,
+  Categories,
+  PopularProducts,
+} from "./components";
+import { client } from "./client";
+import {
+  bannerQuery,
+  productQuery,
+  footerQuery,
+} from "./sanity_barcoblanco/queries";
+import Features from "./components/Features";
+import CallButton from "./components/CallButton";
 
-export default function Home() {
+const Home = async () => {
+  const bannerData = await client.fetch(bannerQuery);
+  const productsData = await client.fetch(productQuery);
+  const footerData = await client.fetch(footerQuery);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <HeroBanner heroBanner={bannerData?.[0]} />
+      <Categories />
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <PopularProducts productsData={productsData} />
+      <Features />
+      <CallButton />
+      <AboutSection />
+    </>
   );
-}
+};
+
+export default Home;
