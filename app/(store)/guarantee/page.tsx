9ca37d077f10d109231bcf9f611e48fda4b41c9b
yaroslav gubich <yar.mansens@gmail.com>
2025-02-11 13:31:07 +0100

    Backup commit at 2025-02-11 13:31:07

--- app/(store)/guarantee/page.tsx
+++ app/(store)/guarantee/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic"; // Prevents static pre-rendering
+
 import React from "react";
 import { Box, Typography, Container } from "@mui/material";
 import { styled } from "@mui/material/styles";
