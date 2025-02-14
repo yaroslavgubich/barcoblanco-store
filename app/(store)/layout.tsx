c668b594500f78c7139791072ad283f975b3276a
yaroslav gubich <yar.mansens@gmail.com>
2025-02-14 03:24:04 +0100

    fix vercel build:
    - fixed vercel build
    - removed all occurrences of clerk
    - removed all unused code snippets

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -4,19 +4,16 @@ import "../globals.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
 import { ReactNode } from "react";
-import { ClerkProvider } from "@clerk/nextjs";
 import { CartProvider } from "@/context/CartContext";
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <ClerkProvider>
-      <CartProvider>
-        <div className="app-container">
-          <Navbar />
-          <main className="main-container">{children}</main>
-          <Footer />
-        </div>
-      </CartProvider>
-    </ClerkProvider>
+    <CartProvider>
+      <div className="app-container">
+        <Navbar />
+        <main className="main-container">{children}</main>
+        <Footer />
+      </div>
+    </CartProvider>
   );
 }
